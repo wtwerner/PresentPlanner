bd76146ba180a7c73a9ae15bb4bb95771de433cf
wtwerner <wtwerner@gmail.com>
2020-08-12 22:12:06 -0500

    Create signup view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
     
+    get '/signup' do 
+        if !logged_in?
+            erb :'/users/signup'
+        else
+            redirect to '/recipients'
+        end 
+    end
+
+
 end
