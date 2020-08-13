87bfbc8f23a704802d65be7b806fc52d6c3c9fe6
wtwerner <wtwerner@gmail.com>
2020-08-12 22:37:57 -0500

    Create signup and login framework

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,3 +1,12 @@
 class RecipientsController < ApplicationController
-    
+
+    get '/recipients' do
+        if logged_in? 
+            @recipients = Recipient.all
+            erb :'recipients/recipients'
+        else
+            redirect to '/login'   
+        end 
+    end
+
 end
