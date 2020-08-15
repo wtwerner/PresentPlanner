462485e273e39b053d653e4101698b8195d72b14
wtwerner <wtwerner@gmail.com>
2020-08-15 18:04:51 -0500

    Add login POST and account route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,42 @@
 class UsersController < ApplicationController
     
     get '/signup' do 
-        if !Helpers.is_logged_in?(session)
-            erb :'/users/signup'
-        else
-            puts session
-            puts Helpers.is_logged_in?(session)
-            puts session[:user_id]
-            redirect to '/recipients'
-        end 
+      if !Helpers.is_logged_in?(session)
+          erb :'/users/signup'
+      else
+          puts session
+          puts Helpers.is_logged_in?(session)
+          puts session[:user_id]
+          redirect to '/recipients'
+      end 
     end
 
     post '/signup' do
-        user = User.new(:email => params[:email], :password => params[:password])
-        if user.save
-          redirect '/login'
-        else
-          redirect '/failure'
-        end
+      user = User.new(:email => params[:email], :password => params[:password])
+      if user.save
+        redirect '/login'
+      else
+        redirect '/failure'
       end
+    end
 
-      get 'login' do
-        erb :'/users/login'
+    get '/login' do
+      erb :'/users/login'
+    end
+
+    post '/login' do
+      user = User.find_by(:email => params[:email])
+    
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/account'
+      else
+        redirect '/failure'
       end
+    end
 
-
+    get '/account' do
+      erb :'/users/account'
+    end
+    
 end
