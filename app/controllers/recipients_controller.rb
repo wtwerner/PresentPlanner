a7200a2b70c05801365cbd92c9eadc5ee1125d13
wtwerner <wtwerner@gmail.com>
2020-08-16 15:51:39 -0500

    Adds edit recipient form

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -19,7 +19,7 @@ class RecipientsController < ApplicationController
 
     post '/recipients' do
         if Helpers.is_logged_in?(session)
-            if params[:content] == ""
+            if params[:name] == ""
                 redirect to '/recipients/new'
             else 
                 @recipient = Recipient.new(params)
@@ -35,6 +35,15 @@ class RecipientsController < ApplicationController
         end 
     end
 
+    get '/recipients/:id' do
+        if Helpers.is_logged_in?(session)  
+            @recipient = Recipient.find_by_id(params[:id])
+            erb :'/recipients/show_recipient'
+        else 
+            redirect to '/login'
+        end  
+    end 
+
     get '/recipients/:id/edit' do 
         @recipient = Recipient.find_by_id(params[:id])
         if Helpers.is_logged_in?(session) && Helpers.current_user(session) == @recipient.user
@@ -43,5 +52,43 @@ class RecipientsController < ApplicationController
             redirect to '/login'
         end 
     end 
+
+    patch '/recipients/:id' do
+        if Helpers.is_logged_in?(session)
+          if params[:name] == ""
+            redirect to "/recipients/#{params[:id]}/edit"
+          else
+            @recipient = Recipient.find_by_id(params[:id])
+            if @recipient && @recipient.user == Helpers.current_user(session)
+              if @recipient.update(
+                  name: params[:name], 
+                  relationship: params[:relationship],
+                  birth_month: params[:birth_month],
+                  birth_day: params[:birth_day]
+                  )
+                redirect to "/recipients/#{@recipient.id}"
+              else
+                redirect to "/recipients/#{@recipient.id}/edit"
+              end
+            else
+              redirect to '/recipients'
+            end
+          end
+        else
+          redirect to '/login'
+        end
+      end
+
+    delete '/recipients/:id/delete' do 
+        if Helpers.is_logged_in?(session)
+            @recipient = Recipient.find_by_id(params[:id])
+            if @recipient && @recipient.user == Helpers.current_user(session)
+                @recipient.delete
+            end  
+            redirect to '/recipients'
+        else 
+            redirect to '/login'
+        end 
+    end   
 
 end
