ad1346d33a237f24c140dc2badac28245edab618
wtwerner <wtwerner@gmail.com>
2020-08-16 17:54:48 -0500

    Create new list route and form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,29 @@
 class ListsController < ApplicationController
     
+    get '/lists/new' do
+        if Helpers.is_logged_in?(session)
+            @recipients = Recipient.all
+            erb :'lists/new'
+        else
+            redirect to '/login'   
+        end 
+    end
+
+    post '/lists' do
+        if Helpers.is_logged_in?(session)
+            if params[:name] == ""
+                redirect to '/lists/new'
+            else 
+                @list = List.new(params)
+                if @list.save
+                    redirect to "/lists/#{@list.id}"
+                else 
+                    redirect to '/lists/new'
+                end 
+            end 
+        else 
+            redirect to '/login'
+        end 
+    end
+
 end
