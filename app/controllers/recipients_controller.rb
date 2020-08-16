ae2071cf746efbb4e1aaacb9f3205f469247e92e
wtwerner <wtwerner@gmail.com>
2020-08-16 14:54:43 -0500

    Create post route for new recipient

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -17,4 +17,31 @@ class RecipientsController < ApplicationController
         end 
     end
 
+    post '/recipients' do
+        if Helpers.is_logged_in?(session)
+            if params[:content] == ""
+                redirect to '/recipients/new'
+            else 
+                @recipient = Recipient.new(params)
+                @recipient.user = Helpers.current_user(session)
+                if @recipient.save
+                    redirect to "/recipients/#{@recipient.id}"
+                else 
+                    redirect to '/recipients/new'
+                end 
+            end 
+        else 
+            redirect to '/login'
+        end 
+    end
+
+    get '/recipients/:id' do
+        if Helpers.is_logged_in?(session)
+            @recipient = Recipient.find_by_id(params[:id])
+            erb :'/recipients/show_recipient'
+        else 
+            redirect to '/login'
+        end  
+    end 
+
 end
