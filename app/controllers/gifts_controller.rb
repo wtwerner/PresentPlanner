c9f8d99bc567364bffa6c02f260562ef5db2ddb5
wtwerner <wtwerner@gmail.com>
2020-08-17 20:13:06 -0500

    Create controller routes

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,3 +1,69 @@
 class GiftsController < ApplicationController
-    
+
+    get '/gifts' do
+        if Helpers.is_logged_in?(session)
+            @gifts = Gift.all
+            erb :'gifts/gifts'
+        else
+            redirect to '/login'   
+        end   
+    end
+
+    get '/gifts/new' do
+        if Helpers.is_logged_in?(session)
+            @recipients = Recipient.all
+            erb :'gifts/new'
+        else
+            redirect to '/login'   
+        end 
+    end
+
+    post '/gifts' do
+        if Helpers.is_logged_in?(session)
+            if params[:name] == ""
+                redirect to '/gifts/new'
+            else 
+                @gift = Gift.new(params)
+                if @gift.save
+                    redirect to "/gifts/#{@gift.id}"
+                else 
+                    redirect to '/gifts/new'
+                end 
+            end 
+        else 
+            redirect to '/login'
+        end 
+    end
+
+    get '/gifts/:id' do
+        if Helpers.is_logged_in?(session)  
+            @gift = Gift.find_by_id(params[:id])
+            erb :'/gifts/show_gift'
+        else 
+            redirect to '/login'
+        end  
+    end 
+
+    get '/gifts/:id/edit' do 
+        @gift = Gift.find_by_id(params[:id])
+        @recipients = Recipient.all
+        if Helpers.is_logged_in?(session) && Helpers.current_user(session) == @gift.list.recipient.user
+            erb :'/gifts/edit'
+        else 
+            redirect to '/login'
+        end 
+    end 
+
+    delete '/gifts/:id/delete' do 
+        if Helpers.is_logged_in?(session)
+            @gift = Gift.find_by_id(params[:id])
+            if @gift && @gift.list.recipient.user == Helpers.current_user(session)
+                @gift.delete
+            end  
+            redirect to '/gifts'
+        else 
+            redirect to '/login'
+        end 
+    end   
+
 end
